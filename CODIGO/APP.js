18ad2d5adf302a80573e19a7af5806d621f7721f
SAMUEL™ <107000099+VILHALVA@users.noreply.github.com>
2024-08-19 13:37:04 -0500

    Add files via upload

---
+++ CODIGO/APP.js
@@ -0,0 +1,47 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+    let filePath = '.' + req.url;
+
+    if (filePath === './') {
+        filePath = './index.html'; 
+    }
+
+    const extname = String(path.extname(filePath)).toLowerCase();
+    const mimeTypes = {
+        '.html': 'text/html',
+        '.js': 'text/javascript',
+        '.css': 'text/css',
+        '.json': 'application/json',
+        '.png': 'image/png',
+        '.jpg': 'image/jpeg',
+        '.gif': 'image/gif',
+        '.wav': 'audio/wav',
+        '.mp4': 'video/mp4',
+        '.woff': 'application/font-woff',
+        '.ttf': 'application/font-ttf',
+        '.eot': 'application/vnd.ms-fontobject',
+        '.otf': 'application/font-otf',
+        '.svg': 'application/image/svg+xml',
+    };
+
+    const contentType = mimeTypes[extname] || 'application/octet-stream';
+
+    fs.readFile(filePath, (err, data) => {
+        if (err) {
+            res.writeHead(404, { 'Content-Type': 'text/plain' });
+            res.end("Arquivo não encontrado.");
+            return;
+        }
+
+        res.writeHead(200, { 'Content-Type': contentType });
+        res.end(data, 'utf-8');
+    });
+});
+
+const PORT = process.env.PORT || 8000; 
+server.listen(PORT, () => {
+    console.log(`SERVIDOR RODANDO EM http://localhost:${PORT}/`);
+});
